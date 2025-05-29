75b75384122e57dd8727bacca6a60f5b66e80d50
Tony Okeke <tonykabilanokeke@gmail.com>
2025-05-28 21:45:52 -0400

    build: add default app for linux

--- flake.nix
+++ flake.nix
@@ -7,26 +7,26 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
   in {
-    # packages.${system}.default = pkgs.buildGoModule rec {
-    #   pname = "dictator";
-    #   version = "latest";
-    #   src = ./.;
-    #
-    #   vendorHash = "sha256-NJdXa+h3VjTrpjs1B2xi9hWyCZ9YIUry3s3zUeCzpCw=";
-    #
-    #   buildPhase = ''
-    #     runHook preBuild
-    #     make build
-    #     runHook postBuild
-    #   '';
-    #
-    #   installPhase = ''
-    #     runHook preInstall
-    #     mkdir -p $out/bin
-    #     cp build/dictator $out/bin/
-    #     runHook postInstall
-    #   '';
-    # };
+    packages.${system}.default = pkgs.buildGoModule rec {
+      pname = "dictator";
+      version = "latest";
+      src = ./.;
+
+      vendorHash = "sha256-NJdXa+h3VjTrpjs1B2xi9hWyCZ9YIUry3s3zUeCzpCw=";
+
+      buildPhase = ''
+        runHook preBuild
+        make build
+        runHook postBuild
+      '';
+
+      installPhase = ''
+        runHook preInstall
+        mkdir -p $out/bin
+        cp build/dictator $out/bin/
+        runHook postInstall
+      '';
+    };
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
         go
